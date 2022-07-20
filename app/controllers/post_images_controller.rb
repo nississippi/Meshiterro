40a053c65cec990955caf53c6e0d38694c27d38d
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-20 09:29:52 +0000

    [5-21]バリデーションと権限の設定

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,15 +2,18 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     #post_imageのuser_idを今ログインしているユーザーのidに指定する
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
-  
+
   def index
     @post_images = PostImage.all
   end
@@ -19,7 +22,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
@@ -27,7 +30,7 @@ class PostImagesController < ApplicationController
   end
 
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
